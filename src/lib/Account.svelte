8ac380aee57430a3321a175189b4c4ce580fa1c6
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-05-16 17:13:30 +0200

    feat: :sparkles: add Account and Updated components

---
+++ src/lib/Account.svelte
@@ -0,0 +1,24 @@
+<script>
+	// account is a prop which can take in data from the parent like this <Account account={account} /> or shorthand <Account {account}/>
+	export let account;
+
+	const { name, pass, accountNumber, money } = account;
+</script>
+
+<div class="m-4 w-[25%] rounded-lg bg-blue-500 p-4 text-slate-100">
+	<p class="text-xl">
+		{name}
+	</p>
+	<p>
+		{pass}
+	</p>
+	<p>
+		{accountNumber}
+	</p>
+	<p>
+		{money} SEK
+	</p>
+</div>
+
+<style>
+</style>
---
+++ src/lib/Updated.svelte
@@ -0,0 +1,31 @@
+<script>
+	import { onMount } from "svelte";
+	import { fly } from "svelte/transition";
+	export let method;
+
+	let show = false;
+	onMount(() => {
+		show = true;
+		setTimeout(() => {
+			show = false;
+		}, 2000);
+	});
+</script>
+
+<main
+	transition:fly={{ y: 50 }}
+	class:bg-green-200={method === "POST"}
+	class:bg-red-200={method === "DELETE"}
+	class:bg-blue-200={method === "PUT"}
+	class="absolute left-[50%] top-[50%] translate-x-[-50%] rounded-xl p-12 text-center">
+	{#if method === "POST"}
+		<h1 class="text-4xl text-green-500">Successfully added new account!</h1>
+	{:else if method === "DELETE"}
+		<h1 class="text-4xl text-red-500">Successfully deleted account!</h1>
+	{:else if method === "PUT"}
+		<h1 class="text-4xl text-blue-500">Successfully updated account!</h1>
+	{/if}
+</main>
+
+<style>
+</style>

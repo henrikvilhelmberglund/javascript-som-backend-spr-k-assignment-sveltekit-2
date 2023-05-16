34d5949fc8956795f2ae65d236a85ae13e30f2cd
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-05-16 17:13:56 +0200

    feat: :sparkles: add +layout.svelte with nav

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,48 @@
 <script>
-	import "@unocss/reset/antfu.css";
+	import { onMount, beforeUpdate } from "svelte";
+	import "@unocss/reset/tailwind.css";
+
+	let show = false;
+	let debug = true;
+	let loadingText = "Loading...";
+	onMount(() => {
+		show = true;
+	});
+	let currentRoute = "";
+	beforeUpdate(() => {
+		currentRoute = window.location.pathname;
+		// console.log(currentRoute);
+	});
+	const routes = {
+		Bankademin: "/",
+		"Add account": "/add-account",
+		"View account": "/view-accounts",
+	};
 </script>
 
-<slot />
+{#if show}
+	<header>
+		<nav class="font-inknut bg-blue-700 p-4">
+			{#each Object.entries(routes) as [name, link]}
+				<a
+					class:!decoration-blue-200={currentRoute === link}
+					class="decoration-offset-6 p-4 pb-0 text-blue-200 underline decoration-transparent hover:text-blue-100 md:text-2xl"
+					href={link}>{name}</a>
+			{/each}
+		</nav>
+	</header>
+	<slot />
+{:else}
+	<div class="flex h-screen w-screen flex-col items-center justify-center">
+		<div
+			class="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
+			role="status">
+			<span
+				class="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]"
+				>Loading...</span>
+		</div>
+		<h1 class="text-3xl">Loading...</h1>
+	</div>
+{/if}
 
 <style uno:preflights uno:safelist global></style>

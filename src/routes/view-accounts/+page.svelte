cc5c0c6b4b7b7b1c02a15eda8e202803c8f58196
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-05-16 17:15:54 +0200

    feat: :sparkles: add view-accounts route

---
+++ src/routes/view-accounts/+page.svelte
@@ -0,0 +1,22 @@
+<script>
+	import Account from "$lib/Account.svelte";
+
+  // get data from +page.server.js
+  export let data;
+</script>
+
+<svelte:head>
+	<title>Bankademin - View accounts</title>
+</svelte:head>
+
+<main>
+	<div class="flex flex-col">
+		<h1 class="m-4 text-xl">View accounts</h1>
+		{#each Object.entries(data.accounts) as [key, account]}
+			<Account {account} />
+		{/each}
+	</div>
+</main>
+
+<style>
+</style>

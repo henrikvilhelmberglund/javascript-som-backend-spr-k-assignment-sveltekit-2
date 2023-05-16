a04c721b8c36cb6785cf0d1993aae50c862954b2
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-05-16 17:14:25 +0200

    feat: :sparkles: add add-account route

---
+++ src/routes/add-account/+page.svelte
@@ -0,0 +1,79 @@
+<script>
+	import Updated from "$lib/Updated.svelte";
+
+	let debug = true;
+	let name;
+	let pass;
+	let money;
+	let error = false;
+	let success = undefined;
+
+	if (debug) {
+		name = "Superman";
+		pass = "Test1234";
+		money = 500;
+	}
+</script>
+
+<svelte:head>
+	<title>Bankademin - Add account</title>
+</svelte:head>
+
+<main class="flex-1 [&>*]:m-4">
+	<form
+		on:submit|preventDefault={async () => {
+			let fetchData;
+			const res = await fetch(`http://localhost:5173/api/accounts/`, {
+				method: "POST",
+				body: JSON.stringify({ name, pass, money }),
+			});
+			if (res.ok) {
+				fetchData = await res.json();
+				console.log(fetchData);
+
+				success = true;
+				setTimeout(() => {
+					success = undefined;
+				}, 1500);
+			} else {
+				error = { status: 401, text: "Unauthorized" };
+			}
+		}}
+		class="m-4 flex w-96 flex-col gap-4">
+		<div class="font-inknut flex flex-col gap-4 md:w-96">
+			<input
+				required
+				type="text"
+				class="outline-solid rounded p-2 outline-1 outline-black"
+				placeholder="Name"
+				id="name"
+				name="name"
+				bind:value={name} />
+			<input
+				required
+				type="password"
+				class="outline-solid rounded p-2 outline-1 outline-black"
+				placeholder="Password"
+				id="password"
+				bind:value={pass}
+				name="password" />
+			<input
+				required
+				type="number"
+				class="outline-solid rounded p-2 outline-1 outline-black"
+				placeholder="Money"
+				id="money"
+				bind:value={money}
+				name="money" />
+			<button
+				class="rounded-lg border border-gray-200 bg-green-400 bg-clip-padding p-8 backdrop-blur-lg backdrop-filter hover:bg-green-300"
+				>Add account</button>
+		</div>
+	</form>
+	{#if success}
+		<Updated method="POST" />
+	{/if}
+</main>
+
+<style>
+</style>

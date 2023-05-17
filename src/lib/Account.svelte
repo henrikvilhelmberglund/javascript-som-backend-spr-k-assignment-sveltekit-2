d818fb96dc3893fda73887e02b51d83388418a06
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-05-17 12:44:28 +0200

    style: :art: rename money to funds

--- src/lib/Account.svelte
+++ src/lib/Account.svelte
@@ -2,23 +2,25 @@
 	// account is a prop which can take in data from the parent like this <Account account={account} /> or shorthand <Account {account}/>
 	export let account;
 
-	const { name, pass, accountNumber, money } = account;
+	const { name, pass, accountNumber, funds } = account;
 </script>
 
-<div class="m-4 w-[25%] rounded-lg bg-blue-500 p-4 text-slate-100">
-	<p class="text-xl">
-		{name}
-	</p>
-	<p>
-		{pass}
-	</p>
-	<p>
-		{accountNumber}
-	</p>
-	<p>
-		{money} SEK
-	</p>
-</div>
+<a class="m-4 inline-block" href="/account/{accountNumber}">
+	<div class="w-fit rounded-lg bg-blue-600 p-4 text-white">
+		<p class="text-xl">
+			{name}
+		</p>
+		<p>
+			{pass}
+		</p>
+		<p>
+			{accountNumber}
+		</p>
+		<p>
+			{funds} SEK
+		</p>
+	</div>
+</a>
 
 <style>
 </style>

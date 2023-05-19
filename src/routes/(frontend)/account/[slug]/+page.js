980119775266c321845f3a7a745b11f5e2f0de96
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-05-19 14:14:17 +0200

    fix: :bug: remove if (browser) code which caused issues, export const prerender = false does the same thing

--- src/routes/(frontend)/account/[slug]/+page.js
+++ src/routes/(frontend)/account/[slug]/+page.js
@@ -1,15 +1,19 @@
-import { browser } from "$app/environment";
+import { error } from "@sveltejs/kit";
+
+export const prerender = false;
 
 /** @type {import('./$types').PageLoad} */
 
 export async function load({ fetch, params }) {
-	if (browser) {
-		const res = await fetch(`http://localhost:5173/api/account/${params.slug}`);
-		const data = await res.json();
-		// can't serialize BSON ID
-
-		return {
-			account: data,
-		};
+	const res = await fetch(`http://localhost:5173/api/account/${params.slug}`);
+	const data = await res.json();
+	if (data === null) {
+		throw error(404, "Page not found");
 	}
+
+	// can't serialize BSON ID
+
+	return {
+		account: data,
+	};
 }

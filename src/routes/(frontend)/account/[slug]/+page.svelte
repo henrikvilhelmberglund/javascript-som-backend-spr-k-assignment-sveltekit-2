a94b7502ad916e3d7a0866464e159e56a4c74c54
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-05-19 14:55:39 +0200

    fix: :art: move deleteAccount function, add delete button, show message when deleted

--- src/routes/(frontend)/account/[slug]/+page.svelte
+++ src/routes/(frontend)/account/[slug]/+page.svelte
@@ -1,14 +1,17 @@
 <script>
 	import { page } from "$app/stores";
-	import { invalidateAll } from "$app/navigation";
+	import { goto, invalidateAll } from "$app/navigation";
 	import Account from "$lib/Account.svelte";
 	import { messageStore } from "$lib/stores.js";
 	import { fly } from "svelte/transition";
 	import Updated from "$lib/Updated.svelte";
 
+	import { deleteAccount } from "$lib/clientFunctions.js";
+
 	const transactionTypes = ["Withdraw", "Deposit"];
 	let state = "Withdraw";
 	let amount;
+	let showAccount = true;
 
 	export let data;
 
@@ -33,69 +36,63 @@
 		}
 	}
 
-	async function deleteAccount() {
-		const res = await fetch(`http://localhost:5173/api/account/${$page.params.slug}`, {
-			method: "DELETE",
-		});
-		console.log(res);
-		if (res.ok) {
-			const data = await res.json();
-			$messageStore = { text: `Successfully deleted account ${data.account.name}`, color: "red" };
-			// rerun load function to update the new funds in the UI
-			await invalidateAll();
-			setTimeout(() => {
-				$messageStore = null;
-			}, 1500);
-		}
-	}
 </script>
 
 <main class="m-4 flex w-fit flex-col">
-	<div class="flex flex-row">
-		<Account account={data.account} />
-		<button on:click={deleteAccount} class="text-4xl">❌</button>
-	</div>
-	<div class="flex justify-between">
-		{#each transactionTypes as transaction}
-			<button
-				on:click={() => (state = transaction)}
-				class:outline-2;shadow-inner,!bg-slate-300={state === transaction}
-				class:shadow-md={state !== transaction}
-				class="m-4 rounded bg-white p-2 shadow-black outline outline-1">{transaction}</button>
-		{/each}
-	</div>
-	<form on:submit={submitData} class="flex flex-col">
-		{#if state === "Withdraw"}
-			<input
-				class="outline-px rounded p-1 outline"
-				bind:value={amount}
-				required
-				type="number"
-				min="1"
-				max={data.account.funds}
-				name=""
-				id="" />
-		{:else if state === "Deposit"}
-			<input
-				class="outline-px rounded p-1 outline"
-				bind:value={amount}
-				required
-				type="number"
-				min="1"
-				max="30000"
-				name=""
-				id="" />
+	<div out:fly={{ y: 50 }}>
+		{#if showAccount}
+			<div class="flex flex-row">
+				<Account account={data.account} />
+				<button
+					on:click={() => {
+            deleteAccount(data.account, messageStore);
+						showAccount = false;
+					}}
+					class="text-4xl">❌</button>
+			</div>
+			<div class="flex justify-between">
+				{#each transactionTypes as transaction}
+					<button
+						on:click={() => (state = transaction)}
+						class:outline-2;shadow-inner,!bg-slate-300={state === transaction}
+						class:shadow-md={state !== transaction}
+						class="m-4 rounded bg-white p-2 shadow-black outline outline-1">{transaction}</button>
+				{/each}
+			</div>
+			<form on:submit={submitData} class="flex flex-col">
+				{#if state === "Withdraw"}
+					<input
+						class="outline-px rounded p-1 outline"
+						bind:value={amount}
+						required
+						type="number"
+						min="1"
+						max={data.account.funds}
+						name=""
+						id="" />
+				{:else if state === "Deposit"}
+					<input
+						class="outline-px rounded p-1 outline"
+						bind:value={amount}
+						required
+						type="number"
+						min="1"
+						max="30000"
+						name=""
+						id="" />
+				{/if}
+				{#if state}
+					<button
+						class:bg-red-500,hover:bg-red-400={state === "Withdraw"}
+						class:bg-green-500,hover:bg-green-400={state === "Deposit"}
+						class="m-4 w-fit self-center rounded p-4 disabled:cursor-not-allowed disabled:opacity-50"
+						disabled={data.account.funds === 0 && state === "Withdraw"}>
+						{state}</button>
+				{/if}
+			</form>
 		{/if}
-		{#if state}
-			<button
-				class:bg-red-500,hover:bg-red-400={state === "Withdraw"}
-				class:bg-green-500,hover:bg-green-400={state === "Deposit"}
-				class="m-4 w-fit self-center rounded p-4 disabled:cursor-not-allowed disabled:opacity-50"
-				disabled={data.account.funds === 0 && state === "Withdraw"}>
-				{state}</button>
+		{#if $messageStore}
+			<Updated />
 		{/if}
-	</form>
-	{#if $messageStore}
-		<Updated />
-	{/if}
+	</div>
 </main>

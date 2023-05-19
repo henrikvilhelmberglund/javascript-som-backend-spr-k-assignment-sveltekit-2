de6f894de6dd18a225ad713db5c0b4fb48523732
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-05-19 14:14:35 +0200

    feat: :sparkles: add error page for account slug route

---
+++ src/routes/(frontend)/account/[slug]/+error.svelte
@@ -0,0 +1,9 @@
+<script>
+	import { page } from "$app/stores";
+</script>
+
+<h1 class="text-3xl">{$page.error.message}</h1>
+<img src="https://http.cat/{$page.status}" alt="cute cat" />
+
+<style>
+</style>

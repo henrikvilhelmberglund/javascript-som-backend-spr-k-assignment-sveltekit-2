553ddb3dcd490cc2c37744399430011402619302
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-05-20 23:36:43 +0200

    feat: :sparkles: add try catch for the server connection
    no idea if this is actually useful but doesn't hurt I guess

--- src/lib/server/mongoConnect.js
+++ src/lib/server/mongoConnect.js
@@ -1,9 +1,14 @@
 import { MongoClient } from "mongodb";
 
 export async function connectToMongoDB() {
-	let client = new MongoClient("mongodb://127.0.0.1:27017");
-	await client.connect();
-	await client.db("admin").command({ ping: 1 });
-	console.info("Pinged your deployment. You successfully connected to MongoDB!");
+	let client;
+	try {
+		client = new MongoClient("mongodb://127.0.0.1:27017");
+		await client.connect();
+		await client.db("admin").command({ ping: 1 });
+		console.info("Pinged your deployment. You successfully connected to MongoDB!");
+	} catch (error) {
+		console.error(error);
+	}
 	return client;
 }

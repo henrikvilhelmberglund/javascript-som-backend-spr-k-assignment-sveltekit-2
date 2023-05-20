94cbc8e9a92ce1b29cd1334d27201ad639865174
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-05-20 21:12:24 +0200

    feat: :sparkles: only show add account form when you are logged in

--- src/routes/(frontend)/add-account/+page.svelte
+++ src/routes/(frontend)/add-account/+page.svelte
@@ -2,6 +2,8 @@
 	import Updated from "$lib/Updated.svelte";
 	import { messageStore } from "$lib/stores";
 
+	export let data;
+
 	let debug = true;
 	let name;
 	let pass;
@@ -13,6 +15,11 @@
 		name = "Personal account";
 		funds = 500;
 	}
+
+	let show = false;
+	if (!data.error) {
+		show = true;
+	}
 </script>
 
 <svelte:head>
@@ -20,46 +27,51 @@
 </svelte:head>
 
 <main class="flex-1 [&>*]:m-4">
-	<form
-		on:submit|preventDefault={async () => {
-			let fetchData;
-			const res = await fetch(`http://localhost:5173/api/accounts/`, {
-				method: "POST",
-				body: JSON.stringify({ name, funds }),
-			});
-			if (res.ok) {
-				fetchData = await res.json();
-				console.log(fetchData);
+	{#if show}
+  <h1 class="my-4 text-xl">Add an account</h1>
+		<form
+			on:submit|preventDefault={async () => {
+				let fetchData;
+				const res = await fetch(`http://localhost:5173/api/accounts/`, {
+					method: "POST",
+					body: JSON.stringify({ name, funds }),
+				});
+				if (res.ok) {
+					fetchData = await res.json();
+					console.log(fetchData);
 
-				$messageStore = { color: "green", text: `Successfully added new account: ${name}!` };
-			} else {
-				error = { status: 401, text: "Unauthorized" };
-			}
-		}}
-		class="m-4 flex w-96 flex-col gap-4">
-		<div class="font-inknut flex flex-col gap-4 md:w-96">
-			<input
-				required
-				type="text"
-				class="outline-solid rounded p-2 outline-1 outline-black"
-				placeholder="Name"
-				id="name"
-				name="name"
-				bind:value={name} />
-			<input
-				required
-				type="number"
-				class="outline-solid rounded p-2 outline-1 outline-black"
-				placeholder="Funds"
-				id="funds"
-				min="0"
-				bind:value={funds}
-				name="funds" />
-			<button
-				class="rounded-lg border border-gray-200 bg-green-400 bg-clip-padding p-8 backdrop-blur-lg backdrop-filter hover:bg-green-300"
-				>Add account</button>
-		</div>
-	</form>
+					$messageStore = { color: "green", text: `Successfully added new account: ${name}!` };
+				} else {
+					error = { status: 401, text: "Unauthorized" };
+				}
+			}}
+			class="m-4 flex w-96 flex-col gap-4">
+			<div class="font-inknut flex flex-col gap-4 md:w-96">
+				<input
+					required
+					type="text"
+					class="outline-solid rounded p-2 outline-1 outline-black"
+					placeholder="Account name"
+					id="name"
+					name="name"
+					bind:value={name} />
+				<input
+					required
+					type="number"
+					class="outline-solid rounded p-2 outline-1 outline-black"
+					placeholder="Funds"
+					id="funds"
+					min="0"
+					bind:value={funds}
+					name="funds" />
+				<button
+					class="rounded-lg border border-gray-200 bg-green-400 bg-clip-padding p-8 backdrop-blur-lg backdrop-filter hover:bg-green-300"
+					>Add account</button>
+			</div>
+		</form>
+	{:else}
+		<h2 class="text-lg">You need to login before adding an account.</h2>
+	{/if}
 </main>
 {#if $messageStore}
 	<Updated />

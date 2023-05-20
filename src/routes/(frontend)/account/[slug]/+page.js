c0462535b48f9538ab44cc7c8612ee256eb184e3
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-05-20 17:44:55 +0200

    fix: :bug: disable ssr for this route, use checkLoginStatus() function

--- src/routes/(frontend)/account/[slug]/+page.js
+++ src/routes/(frontend)/account/[slug]/+page.js
@@ -1,17 +1,24 @@
+import { browser } from "$app/environment";
+import { checkLoginStatus } from "$lib/clientFunctions";
 import { error } from "@sveltejs/kit";
 
 export const prerender = false;
+export const ssr = false;
 
 /** @type {import('./$types').PageLoad} */
 
 export async function load({ fetch, params }) {
+	const auth = await checkLoginStatus();
+	console.log("authenticated", auth.user);
+	if (!auth.user) {
+		throw error(401, "Not authorized");
+	}
 	const res = await fetch(`http://localhost:5173/api/account/${params.slug}`);
 	const data = await res.json();
 	if (data === null) {
 		throw error(404, "Page not found");
 	}
 
-	// can't serialize BSON ID
 
 	return {
 		account: data,

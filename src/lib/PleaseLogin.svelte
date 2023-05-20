d58a5e620835fb140379f9310636e1edacc062f6
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-05-20 22:39:42 +0200

    feat: :sparkles: add PleaseLogin component that tells you to log in

---
+++ src/lib/PleaseLogin.svelte
@@ -0,0 +1,11 @@
+<script>
+</script>
+
+<h2 class="text-lg">
+	You need to
+	<a class="decoration-offset-2 font-semibold underline decoration-blue-500" href="/login">Login</a>
+	<slot />.
+</h2>
+
+<style>
+</style>

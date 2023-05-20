c6cafe2e23bf027797b3d1b7eb166ecdc7a620d2
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-05-20 17:43:59 +0200

    feat: :sparkles: add a beautiful (?) index page

--- src/routes/(frontend)/+page.svelte
+++ src/routes/(frontend)/+page.svelte
@@ -3,8 +3,9 @@
 	import Footer from "$lib/Footer.svelte";
 </script>
 
-<main class="[&>*]:m-4">
-Hello
+<main class="h-[93vh] bg-gradient-to-b from-white to-blue-300">
+	<h1 class="p-4 text-4xl">Welcome to Bankademin!</h1>
+	<h2 class="p-4 text-2xl">With us your funds are safe forever.</h2>
 </main>
 
 <Footer />

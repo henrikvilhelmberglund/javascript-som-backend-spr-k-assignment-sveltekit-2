33c85b006f0373de75b0e096e1a7d08d0a15416a
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-05-20 22:04:39 +0200

    fix: :fire: cleanup

--- src/routes/api/accounts/+server.js
+++ src/routes/api/accounts/+server.js
@@ -1,6 +1,6 @@
 import { getAccountsCollection } from "/src/hooks.server.js";
 import { v4 as uuidv4 } from "uuid";
-import { error, json } from "@sveltejs/kit";
+import { json } from "@sveltejs/kit";
 
 // +server.js files are endpoints, the functions below can be fetch() from /accounts/ (this route) and either GET or POST (also DELETE and PUT if needed)
 
@@ -16,22 +16,9 @@ export async function GET({ url, request, locals }) {
 export async function POST({ url, request, cookies, locals }) {
 	const incomingBody = await request.json();
 	const accountNumber = uuidv4();
-	// const body = result.body;
 	let { ownedBy, name, funds } = incomingBody;
 
-	// let { title, content, date, tags } = Object.fromEntries(formData);
-
 	const result = await accountsCollection.insertOne({ ownedBy, name, accountNumber, funds });
-	// cookies.set("session", uuidv4(), {
-	// 	httpOnly: true,
-	// 	sameSite: "strict",
-	// 	secure: false,
-	// 	path: "/",
-	// 	maxAge: 60 * 60 * 24 * 7,
-	// });
-	// locals = {
-	// 	user: user,
-	// };
-	console.log(locals);
+
 	return json(result);
 }

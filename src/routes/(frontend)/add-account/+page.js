4b33606abbe5b7eb76c97bf20ad6e51dbff3ee9a
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-05-20 21:58:05 +0200

    fix: :fire: disable ssr
    I get confused by the console logs in the server that should only really appear in the browser (there is probably a better way to fix this, maybe if (browser))

--- src/routes/(frontend)/add-account/+page.js
+++ src/routes/(frontend)/add-account/+page.js
@@ -1,5 +1,6 @@
 import { checkLoginStatus } from "$lib/clientFunctions";
 export const prerender = false;
+export const ssr = false;
 
 // this is a file containing a load() function that is used to load data and then return it to be able to use it with for example data.accounts in +page.svelte
 // you could also do everything in +page.svelte but splitting loading of data and presentation of data seems useful sometimes
@@ -10,10 +11,12 @@ export const prerender = false;
 
 export async function load({ fetch }) {
 	const auth = await checkLoginStatus();
+	console.info("auth in add-account", auth);
 	if (!auth.user) {
 		return {
 			// instead of throwing an error I return it to the load function so I can display something else than just an error page
 			error: 401,
 		};
 	}
+	return { auth };
 }
